2bbc0c9d5d0fab3d7697a3e263cc5e1fe9bd55e8
Benjamin-chidera <benjaminchidera72@gmail.com>
2024-08-30 11:24:51 +0100

    refactor: fixed screen size

--- src/components/Auth/SideBar.tsx
+++ src/components/Auth/SideBar.tsx
@@ -1,5 +1,7 @@
 import "./auth-sidebar.css";
 
 export const SideBar = () => {
-  return <main className=" w-[400px]  h-screen sidebar p-7 bg-cover"></main>;
+  return (
+    <main className=" w-[400px]  h-screen bg-contain 2xl:bg-cover sidebar p-7"></main>
+  );
 };
